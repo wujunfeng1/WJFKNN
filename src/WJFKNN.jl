b673b7400e493859c57de3a401d1d2eb0e4d7593
Junfeng Wu <wujunfeng1@gmail.com>
2021-12-24 11:17:28 +1100

    improve performance with large small task seperations

--- src/WJFKNN.jl
+++ src/WJFKNN.jl
@@ -2,11 +2,51 @@ module WJFKNN
 export WjfKNN, knnSearch
 using WJFParallelTask
 
+struct CoverSet
+    mapping::Dict{UInt32,UInt32}
+    subCenters::Vector{UInt32}
+    subCoverSetInfos::Vector{Tuple{UInt32,UInt32,Float64,Float64}}
+    subCoverSets::Vector{CoverSet}
+end
+
+function findCenterSequentially(
+    data::Vector,
+    items::Vector{UInt32},
+    maxNumSamples::UInt32,
+    distFun::Function,
+)::UInt32
+    numItems = length(items)
+    numSamples = min(UInt32(ceil(sqrt(numItems))), maxNumSamples)
+    samples::Set{UInt32} = Set{UInt32}()
+    for idxSample = 1:numSamples
+        sample = (rand(UInt32) % numItems) + 1
+        while sample in samples
+            sample = (rand(UInt32) % numItems) + 1
+        end
+        push!(samples, sample)
+    end
+    sampleLists::Vector{UInt32} = [sample for sample in samples]
+
+    mySmallestSumD = Inf64
+    myBestSample = UInt32(0)
+    for u::UInt32 = 1:UInt32(numSamples)
+        sumD::Float64 = 0.0
+        for v = 1:numSamples
+            sumD += distFun(data[sampleLists[u]], data[sampleLists[v]])
+        end
+        if sumD < mySmallestSumD
+            mySmallestSumD = sumD
+            myBestSample = sampleLists[u]
+        end
+    end
+    return items[myBestSample]
+end
+
 function findCenter(
     data::Vector,
     items::Vector{UInt32},
     maxNumSamples::UInt32,
-    distSqrFun::Function,
+    distFun::Function,
 )::UInt32
     numItems = length(items)
     numSamples = min(UInt32(ceil(sqrt(numItems))), maxNumSamples)
@@ -21,37 +61,37 @@ function findCenter(
     sampleLists::Vector{UInt32} = [sample for sample in samples]
 
     function mapFun(u0::UInt32, u1::UInt32)::Tuple{Float64, UInt32}
-        mySmallestSumDSqr = Inf64
+        mySmallestSumD = Inf64
         myBestSample = UInt32(0)
         for u = u0:u1
-            sumDSqr::Float64 = 0.0
+            sumD::Float64 = 0.0
             for v = 1:numSamples
-                sumDSqr += distSqrFun(data[sampleLists[u]], data[sampleLists[v]])
+                sumD += distFun(data[sampleLists[u]], data[sampleLists[v]])
             end
-            if sumDSqr < mySmallestSumDSqr
-                mySmallestSumDSqr = sumDSqr
+            if sumD < mySmallestSumD
+                mySmallestSumD = sumD
                 myBestSample = sampleLists[u]
             end
         end # u
-        return (mySmallestSumDSqr, myBestSample)
+        return (mySmallestSumD, myBestSample)
     end
 
     function reduceFun(values::Vector{Tuple{Float64, UInt32}}
         )::Tuple{Float64, UInt32}
-        mySmallestSumDSqr = Inf64
+        mySmallestSumD = Inf64
         myBestSample = UInt32(0)
-        for (sumDSqr, sample) in values
-            if sumDSqr < mySmallestSumDSqr
-                mySmallestSumDSqr = sumDSqr
+        for (sumD, sample) in values
+            if sumD < mySmallestSumD
+                mySmallestSumD = sumD
                 myBestSample = sample
             end
         end
-        return (mySmallestSumDSqr, myBestSample)
+        return (mySmallestSumD, myBestSample)
     end
 
     bestSample = 0
     while bestSample == 0
-        (smallestSumDSqr, bestSample) = mapReduce(
+        (smallestSumD, bestSample) = mapReduce(
             UInt32(1), numSamples, UInt32(100),
             mapFun, reduceFun, (Inf64, UInt32(0)))
     end
@@ -59,47 +99,71 @@ function findCenter(
     return items[bestSample]
 end
 
+function findFarthestItemSequentially(
+    data::Vector,
+    items::Vector{UInt32},
+    center,
+    subCenters::Vector{UInt32},
+    distFun::Function,
+)::UInt32
+    numItems = length(items)
+    myFarthestdist = 0.0
+    myFarthestItem = 0
+    for u::UInt32 = 1:UInt32(numItems)
+        item = items[u]
+        mydist = distFun(data[item], center)
+        for j in subCenters
+            mydist = min(mydist, distFun(data[item], data[j]))
+        end
+        if mydist > myFarthestdist
+            myFarthestItem = items[u]
+            myFarthestdist = mydist
+        end
+    end # u
+    return myFarthestItem
+end
+
 function findFarthestItem(
     data::Vector,
     items::Vector{UInt32},
     center,
     subCenters::Vector{UInt32},
-    distSqrFun::Function,
+    distFun::Function,
 )::UInt32
     function mapFun(u0::UInt32, u1::UInt32)::Tuple{Float64,UInt32}
-        myFarthestDistSqr = 0.0
+        myFarthestdist = 0.0
         myFarthestItem = 0
         for u = u0:u1
             item = items[u]
-            myDistSqr = distSqrFun(data[item], center)
+            mydist = distFun(data[item], center)
             for j in subCenters
-                myDistSqr = min(myDistSqr, distSqrFun(data[item], data[j]))
+                mydist = min(mydist, distFun(data[item], data[j]))
             end
-            if myDistSqr > myFarthestDistSqr
+            if mydist > myFarthestdist
                 myFarthestItem = items[u]
-                myFarthestDistSqr = myDistSqr
+                myFarthestdist = mydist
             end
         end # u
-        return (myFarthestDistSqr, myFarthestItem)
+        return (myFarthestdist, myFarthestItem)
     end
 
     function reduceFun(values::Vector{Tuple{Float64,UInt32}}
         )::Tuple{Float64,UInt32}
-        myFarthestDistSqr = 0.0
+        myFarthestdist = 0.0
         myFarthestItem = 0
-        for (distSqr, item) in values
-            if distSqr > myFarthestDistSqr
-                myFarthestDistSqr = distSqr
+        for (dist, item) in values
+            if dist > myFarthestdist
+                myFarthestdist = dist
                 myFarthestItem = item
             end
         end
-        return (myFarthestDistSqr, myFarthestItem)
+        return (myFarthestdist, myFarthestItem)
     end
 
     numItems = length(items)
     farthestItem = 0
     while farthestItem == 0
-        (farthestDistSqr, farthestItem) = mapReduce(
+        (farthestdist, farthestItem) = mapReduce(
             UInt32(1), UInt32(numItems), UInt32(100),
             mapFun, reduceFun, (0.0, UInt32(0))
         )
@@ -107,26 +171,68 @@ function findFarthestItem(
     return farthestItem
 end
 
+function fillSubCenters!(
+    subCenters::Vector{UInt32},
+    data::Vector,
+    items::Vector{UInt32},
+    maxNumSamples::UInt32,
+    numPartitionsPerNode::UInt32,
+    distFun::Function
+)
+    center = data[findCenterSequentially(data, items, maxNumSamples, distFun)]
+    while length(subCenters) < numPartitionsPerNode
+        push!(subCenters, findFarthestItemSequentially(
+            data, items, center, subCenters, distFun))
+    end
+end
+
 function findSubCenters(
     data::Vector,
     items::Vector{UInt32},
     maxNumSamples::UInt32,
     numPartitionsPerNode::UInt32,
-    distSqrFun::Function
+    distFun::Function
 )::Vector{UInt32}
-    center = data[findCenter(data, items, maxNumSamples, distSqrFun)]
+    center = data[findCenter(data, items, maxNumSamples, distFun)]
     subCenters = Vector{UInt32}()
     while length(subCenters) < numPartitionsPerNode
-        push!(subCenters, findFarthestItem(data, items, center, subCenters, distSqrFun))
+        push!(subCenters, findFarthestItem(data, items, center, subCenters, distFun))
     end
     return subCenters
+end
+
+function fillSubCoverSetInfos!(
+    subCoverSetInfos::Vector{Tuple{UInt32,UInt32,Float64,Float64}},
+    data::Vector,
+    items::Vector{UInt32},
+    subCenters::Vector{UInt32},
+    distFun::Function,
+)
+    numItems = length(items)
+    mySubCenterDists = fill(0.0, (length(subCenters),))
+    mySubCenterRanks = Vector{UInt32}(undef, length(subCenters))
+    for u::UInt32 = 1:UInt32(numItems)
+        item = items[u]
+        for i::UInt32 = 1:length(subCenters)
+            mySubCenterRanks[i] = i
+            mySubCenterDists[i] =
+                distFun(data[item], data[subCenters[i]])
+        end
+        sort!(mySubCenterRanks, by = x -> mySubCenterDists[x])
+        subCoverSetInfos[u] = (
+            mySubCenterRanks[1],
+            mySubCenterRanks[2],
+            mySubCenterDists[mySubCenterRanks[1]],
+            mySubCenterDists[mySubCenterRanks[2]],
+        )
+    end
 end
 
 function computeSubCoverSetInfos(
     data::Vector,
     items::Vector{UInt32},
     subCenters::Vector{UInt32},
-    distSqrFun::Function,
+    distFun::Function,
 )::Vector{Tuple{UInt32,UInt32,Float64,Float64}}
     numItems = length(items)
     subCoverSetInfos::Vector{Tuple{Int32,Int32,Float64,Float64}} =
@@ -139,7 +245,7 @@ function computeSubCoverSetInfos(
             for i::UInt32 = 1:length(subCenters)
                 mySubCenterRanks[i] = i
                 mySubCenterDists[i] =
-                    distSqrFun(data[item], data[subCenters[i]])
+                    distFun(data[item], data[subCenters[i]])
             end
             sort!(mySubCenterRanks, by = x -> mySubCenterDists[x])
             subCoverSetInfos[u] = (
@@ -171,11 +277,42 @@ function getSubCoverItems(
     return result
 end
 
-struct CoverSet
-    mapping::Dict{UInt32,UInt32}
-    subCenters::Vector{UInt32}
-    subCoverSetInfos::Vector{Tuple{UInt32,UInt32,Float64,Float64}}
-    subCoverSets::Vector{CoverSet}
+function fillCoverSet!(
+    coverSet::CoverSet,
+    data::Vector,
+    items::Vector{UInt32},
+    maxNumSamples::UInt32,
+    numPartitionsPerNode::UInt32,
+    distFun::Function,
+    smallSubCoverSets::Vector{Tuple{CoverSet, Vector{UInt32}}},
+)
+    fillSubCenters!(
+        coverSet.subCenters,
+        data,
+        items,
+        maxNumSamples,
+        numPartitionsPerNode,
+        distFun
+    )
+    fillSubCoverSetInfos!(
+        coverSet.subCoverSetInfos,
+        data,
+        items,
+        coverSet.subCenters,
+        distFun,
+    )
+    subCoverItems::Vector{Vector{UInt32}} =
+        getSubCoverItems(items, coverSet.subCoverSetInfos, numPartitionsPerNode)
+    for i = 1:numPartitionsPerNode
+        coverSet.subCoverSets[i] = CoverSet(
+            data,
+            subCoverItems[i],
+            maxNumSamples,
+            numPartitionsPerNode,
+            distFun,
+            smallSubCoverSets
+        )
+    end
 end
 
 function CoverSet(
@@ -183,28 +320,40 @@ function CoverSet(
     items::Vector{UInt32},
     maxNumSamples::UInt32,
     numPartitionsPerNode::UInt32,
-    distSqrFun::Function
+    distFun::Function,
+    smallSubCoverSets::Vector{Tuple{CoverSet, Vector{UInt32}}},
 )
+    numItems = length(items)
+    numData = length(data)
     mapping::Dict{UInt32,UInt32} = Dict{UInt32,UInt32}()
-    for i = 1:length(items)
+    for i = 1:numItems
         mapping[items[i]] = i
     end
+    smallSubCoverSetSize = maxNumSamples * UInt32(sqrt(numPartitionsPerNode))
     if length(items) <= maxNumSamples
         return CoverSet(
         mapping,
         Vector{UInt32}(),
         Vector{Tuple{UInt32,UInt32,Float64,Float64}}(),
         Vector{CoverSet}())
+    elseif numData > numItems && numItems <= smallSubCoverSetSize
+        coverSet = CoverSet(
+        mapping,
+        Vector{UInt32}(),
+        Vector{Tuple{UInt32,UInt32,Float64,Float64}}(undef, numItems),
+        Vector{CoverSet}(undef, numPartitionsPerNode))
+        push!(smallSubCoverSets, (coverSet,items))
+        return coverSet
     else
         subCenters = findSubCenters(
             data,
             items,
             maxNumSamples,
             numPartitionsPerNode,
-            distSqrFun
+            distFun
         )
         subCoverSetInfos =
-            computeSubCoverSetInfos(data, items, subCenters, distSqrFun)
+            computeSubCoverSetInfos(data, items, subCenters, distFun)
         subCoverItems::Vector{Vector{UInt32}} =
             getSubCoverItems(items, subCoverSetInfos, numPartitionsPerNode)
         subCoverSets = [
@@ -213,7 +362,8 @@ function CoverSet(
                 subCoverItems[i],
                 maxNumSamples,
                 numPartitionsPerNode,
-                distSqrFun
+                distFun,
+                smallSubCoverSets
             ) for i = 1:numPartitionsPerNode
         ]
         return CoverSet(mapping, subCenters, subCoverSetInfos, subCoverSets)
@@ -222,30 +372,80 @@ end
 
 struct WjfKNN
     data::Vector
-    distSqrFun::Function
+    distFun::Function
     maxNumSamples::UInt32
     numPartitionsPerNode::UInt32
     entry::CoverSet
 end
 
+function finishSmallSubCoverSets(
+    smallSubCoverSets::Vector{Tuple{CoverSet, Vector{UInt32}}},
+    data::Vector,
+    maxNumSamples::UInt32,
+    numPartitionsPerNode::UInt32,
+    distFun::Function,
+    )::Vector{Tuple{CoverSet, Vector{UInt32}}}
+    function mapFun(u0::UInt32,u1::UInt32)
+        myResult = Vector{Tuple{CoverSet, Vector{UInt32}}}()
+        for u in u0:u1
+            subCoverSet = smallSubCoverSets[u][1]
+            items = smallSubCoverSets[u][2]
+            fillCoverSet!(
+                subCoverSet,
+                data,
+                items,
+                maxNumSamples,
+                numPartitionsPerNode,
+                distFun,
+                myResult,
+                )
+        end
+        return myResult
+    end
+    function blockPrefixFun(x0,xs)
+        return xs
+    end
+    result = mapPrefix(
+        UInt32(1),
+        UInt32(length(smallSubCoverSets)),
+        UInt32(1),
+        mapFun,
+        blockPrefixFun,
+        Vector{Tuple{CoverSet, Vector{UInt32}}}(),
+    )
+    return result
+end
+
 function WjfKNN(
     data::Vector,
-    distSqrFun::Function,
-    maxNumSamples::UInt32 = UInt32(1000),
+    distFun::Function,
+    maxNumSamples::UInt32 = UInt32(256),
     numPartitionsPerNode::UInt32 = UInt32(16),
 )
-    return WjfKNN(
+    smallSubCoverSets = Vector{Tuple{CoverSet, Vector{UInt32}}}()
+    entry = CoverSet(
         data,
-        distSqrFun,
+        [i for i::UInt32 = 1:length(data)],
         maxNumSamples,
         numPartitionsPerNode,
-        CoverSet(
+        distFun,
+        smallSubCoverSets,
+    )
+    while length(smallSubCoverSets) > 0
+        smallSubCoverSets = finishSmallSubCoverSets(
+            smallSubCoverSets,
             data,
-            [i for i::UInt32 = 1:length(data)],
             maxNumSamples,
             numPartitionsPerNode,
-            distSqrFun
-        ),
+            distFun,
+        )
+    end
+    return WjfKNN(
+        data,
+        distFun,
+        maxNumSamples,
+        numPartitionsPerNode,
+        entry,
     )
 end
 
@@ -274,8 +474,8 @@ function knnSearch(wjfknn::WjfKNN, i::UInt32, k::UInt32,
         if neighbor == i
             continue
         end
-        distSqr = wjfknn.distSqrFun(wjfknn.data[i], wjfknn.data[neighbor])
-        result = (distSqr, neighbor)
+        dist = wjfknn.distFun(wjfknn.data[i], wjfknn.data[neighbor])
+        result = (dist, neighbor)
         insert!(results, searchsortedfirst(results, result), result)
         if length(results) > k
             pop!(results)
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ include("../src/WJFKNN.jl")
 #import WJFKNN:WjfKNN, knnSearch
 using WJFParallelTask
 
+begin
 data = Vector{Vector{Float64}}()
 numClusters = 20
 numDims = 10
@@ -14,7 +15,7 @@ for i = 1:numClusters
     push!(clusterSizes, rand(Float64, numDims))
 end
 
-numData = 10000
+numData = 100000
 for i = 1:numData
     clusterID = rand(1:numClusters)
     position = clusterCenters[clusterID] .+
@@ -22,34 +23,45 @@ for i = 1:numData
     push!(data, position)
 end
 
-function euclidDistSqr(u::Vector{Float64}, v::Vector{Float64})
-    return sum((u .- v).^2)
+normalizedData = [v./sqrt(sum(v.*v)) for v in data]
+function euclidDist(u::Vector{Float64}, v::Vector{Float64})
+    return sqrt(sum((u .- v).^2))
 end
 
-function cosDistSqr(u::Vector{Float64}, v::Vector{Float64})
-    return 0.5 - 0.5 * sum(u.*v)^2 /(sum(u.*u) * sum(v.*v))
+function cosDist(u::Vector{Float64}, v::Vector{Float64})
+    return 0.5 - 0.5 * sum(u.*v)
 end
 
-function baselineKNN(distSqrFun::Function)::Vector{Vector{Tuple{Float64,UInt32}}}
+function baselineKNN(distFun::Function)::Vector{Vector{Tuple{Float64,UInt32}}}
     result = Vector{Vector{Tuple{Float64,UInt32}}}(undef, numData)
+    myData = if distFun == cosDist
+        normalizedData
+    else
+        data
+    end
     function mapFun(u1::UInt32, u2::UInt32)
-        distSqrs = Vector{Tuple{Float64,UInt32}}(undef, numData)
+        dists = Vector{Tuple{Float64,UInt32}}(undef, numData)
         for i::UInt32 = u1:u2
             for j::UInt32 = 1:numData
-                distSqrs[j] = (distSqrFun(data[i], data[j]), j)
+                dists[j] = (distFun(myData[i], myData[j]), j)
             end
-            distSqrs[i] = (Inf64, i)
-            sort!(distSqrs)
-            result[i] = distSqrs[1:k]
+            dists[i] = (Inf64, i)
+            sort!(dists)
+            result[i] = dists[1:k]
         end # i
     end
     mapOnly(UInt32(1),UInt32(numData),UInt32(100),mapFun)
     return result
 end
 
-function wjfKNN(distSqrFun::Function)::Vector{Vector{Tuple{Float64,UInt32}}}
-    wjfknn = WJFKNN.WjfKNN(data, distSqrFun)
-    return WJFKNN.knnSearch(wjfknn, k, 0.5)
+function wjfKNNEuclid()::Vector{Vector{Tuple{Float64,UInt32}}}
+    wjfKnn = WJFKNN.WjfKNN(data, euclidDist)
+    return WJFKNN.knnSearch(wjfKnn, k, 0.5)
+end
+
+function wjfKNNCos()::Vector{Vector{Tuple{Float64,UInt32}}}
+    wjfKnn = WJFKNN.WjfKNN(normalizedData, cosDist)
+    return WJFKNN.knnSearch(wjfKnn, k, 0.5)
 end
 
 function computeAccuracy(a::Vector{Vector{Tuple{Float64,UInt32}}}, b::Vector{Vector{Tuple{Float64,UInt32}}})
@@ -64,17 +76,21 @@ function computeAccuracy(a::Vector{Vector{Tuple{Float64,UInt32}}}, b::Vector{Vec
 end
 
 @testset "cos distance knn" begin
-    @time a = baselineKNN(cosDistSqr)
-    for round = 1:100
-        @time b = wjfKNN(cosDistSqr)
-        @test computeAccuracy(a, b) > 0.9
+    @time a = baselineKNN(cosDist)
+    for round = 1:10
+        println("round = $round")
+        @time b = wjfKNNCos()
+        @test computeAccuracy(a, b) > 0.85
     end
 end
 
 @testset "euclid distance knn" begin
-    @time a = baselineKNN(euclidDistSqr)
-    for round = 1:100
-        @time b = wjfKNN(euclidDistSqr)
+    @time a = baselineKNN(euclidDist)
+    for round = 1:10
+        println("round = $round")
+        @time b = wjfKNNEuclid()
         @test computeAccuracy(a, b) > 0.9
     end
+end
+
 end
